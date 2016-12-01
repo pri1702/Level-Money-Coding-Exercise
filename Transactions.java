c2da6b184f185419dce32ae46ade427ee037164e
pri1702 <pri1702@gmail.com>
2016-11-30 23:41:34 -0500

    Add files via upload

--- Transactions.java
+++ Transactions.java
@@ -1,10 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Transactions {
 
-//Load Transactions
-	void getAllTransactions() throws MalformedURLException{
-		URL uri = new URL("https://2016.api.levelmoney.com/api/v2/core/get-all-transactions");
+	public static void main(String[] args) {
+
+	  try {
+
+		URL url = new URL("https://2016.api.levelmoney.com/api/v2/core/get-all-transactions");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/json");
+
+		if (conn.getResponseCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ conn.getResponseCode());
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+			(conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
+
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+
+	  }
+
 	}
+
 }
